bc63e269bb7021aaa0aad53162d27b74bc78450f
Seu Nome <seu-email@exemplo.com>
2025-08-19 14:20:04 -0300

    fix: corrigir manifest e adicionar roteamento básico

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 
 const App = () => {
-  return <Index />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="*" element={<Index />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
